102591ab6de316bee4c1bc2dd9d1344ab6217ae3
ledouxm <martin.ledoux.pro@gmail.com>
2025-01-09 17:51:02 +0100

    fix: script

--- dump.ps1
+++ dump.ps1
@@ -1,4 +1,5 @@
+Add-Type -AssemblyName System.Windows.Forms
 $process = Start-Process -FilePath ".\Il2CppDumper\Il2CppDumper.exe" -ArgumentList ".\parser\temp\GameAssembly.dll", ".\parser\temp\Dofus_Data\il2cpp_data\Metadata\global-metadata.dat" -PassThru -NoNewWindow
-Start-Sleep -Seconds 30  # Adjust this delay based on how long the dumping takes
+Start-Sleep -Seconds 30
 [System.Windows.Forms.SendKeys]::SendWait("`n")
 $process.WaitForExit()
